c72991d86c0801f3174ffdeacb2c3b34dc6be2b8
Jenazads <felipe.moreno.vera@gmail.com>
2018-07-26 04:58:34 -0500

    init tree

--- trees/gotree_init.go
+++ trees/gotree_init.go
@@ -3,7 +3,6 @@ package gotree
 import "fmt";
 
 func init() {
-  fmt.Println("\n\n====> goTree version 1.0.1 last update 16/07/2018 \n")
-  fmt.Println("====> Init the GoTree library :D\n")
-  fmt.Println("====> by Jenazads.\n\n")
+  fmt.Println("====> Init GoTree, a Tree Data Structure Library :D")
+  fmt.Println("====> by Jenazads.\n")
 }
